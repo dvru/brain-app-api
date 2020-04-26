6b87552fb53c7d06ac2a1e8c7aa87569fb474e8e
dvru <damvaru4@gmail.com>
2020-04-26 14:47:43 -0500

    updating db

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-curved-74537',
       user : 'daminivaru',
       password : '',
       database : 'brain-app'
